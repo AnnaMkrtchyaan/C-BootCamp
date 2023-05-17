0575d5f4dcf803db92a9b0c596cc9599eb265839
AnnaMkrtchyaan <133821329+AnnaMkrtchyaan@users.noreply.github.com>
2023-05-17 11:49:35 +0400

    Add files via upload

---
+++ Anna-Homework2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Anna_Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //double y;
+            //for (double x = 2.4; x <= 7.6; x += 0.2)
+            //{
+            //    y = Math.Tan(2 * x + x * x);
+            //}
+            //Console.WriteLine("Answer=" + y);
+
+            //Console.Write("Enter number:");
+            //double x=double.Parse(Console.ReadLine());
+            //double y;
+            //if (x >= -5 && x <= 5)
+            //{
+            //    y = Math.Pow(x, 2)+4*Math.Pow(x, 8);
+            //}
+            //else y = 0;
+            //Console.WriteLine("Answer=" + y);
+
+            //Console.Write("Enter number:");
+            //double x = double.Parse(Console.ReadLine());
+            //double pow_tt;
+            //if (x >= 10)
+            //{
+            //    pow_tt = Math.Pow(x + 5, 3);
+            //}
+            //else pow_tt = Math.Pow(x, 2);
+            //Console.WriteLine("Answer=" + pow_tt);
+
+
+        }
+    }
+}
